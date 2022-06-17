176e0988ca48d72b17de7dc3417a7918d8e203d6
Zecheng Qian <qian0102@umn.edu>
2022-06-17 14:12:11 -0500

    Complete 02_04

--- practice/main.go
+++ practice/main.go
@@ -4,13 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
+	fmt.Print("Enter a number: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
-
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of a number: ", aFloat)
+	}
 }
