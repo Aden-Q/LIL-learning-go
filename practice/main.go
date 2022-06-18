24a9a8e0126b7ea48722faf8e8f7d46b6b81e81b
Zecheng Qian <qian0102@umn.edu>
2022-06-18 09:10:33 -0500

    Finish 02_07

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
 	fmt.Println("Dates and times")
-
+	n := time.Now()
+	fmt.Println("The current time is: ", n)
+	t := time.Date(2022, time.June, 18, 9, 8, 0, 0, time.UTC)
+	fmt.Println("", t)
+	fmt.Println(t.Format(time.ANSIC))
 }
