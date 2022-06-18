7c1b90b98046d1fc0ce8ac3127890c2e9b9f8905
Zecheng Qian <qian0102@umn.edu>
2022-06-18 12:14:06 -0500

    Finish 05_03

--- calculator/main.go
+++ calculator/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,23 +12,39 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("Value 1: ")
-	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
+	float1 := readFloat(reader)
 
 	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	float2 := readFloat(reader)
+
+	fmt.Print("Select an operation (+ - * /):")
+	input3, _ := reader.ReadString('\n')
+	op := strings.TrimSpace(input3)
+	result := calTwoValues(float1, float2, op)
+	fmt.Printf("The result is %f\n", result)
+}
+
+func readFloat(reader *bufio.Reader) float64 {
+	input, _ := reader.ReadString('\n')
+	val, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
 	if err != nil {
 		fmt.Println(err)
-		panic("Value 2 must be a number")
+		panic("Value must be a number")
 	}
+	return val
+}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
-
+func calTwoValues(val1, val2 float64, op string) float64 {
+	var result float64
+	switch op {
+	case "+":
+		result = val1 + val2
+	case "-":
+		result = val1 - val2
+	case "*":
+		result = val1 * val2
+	case "/":
+		result = val1 / val2
+	}
+	return result
 }
