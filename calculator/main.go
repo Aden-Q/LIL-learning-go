b78d265cba178d095b5d8ef4c552caf798f7f280
Zecheng Qian <qian0102@umn.edu>
2022-06-18 12:31:59 -0500

    Finish 06_01

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "Hello from Go"
+	file, err := os.Create("sample.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote %v characters to file %v\n", length, "sample.txt")
+	defer file.Close()
+	defer readFile("sample.txt")
+}
+
+func readFile(filename string) {
+	content, err := ioutil.ReadFile(filename)
+	checkError(err)
+	fmt.Println("Read from file:", string(content))
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
