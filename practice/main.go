0031176c66c8854308cdb81fa8dc11460a1ec83f
Zecheng Qian <qian0102@umn.edu>
2022-06-18 11:56:00 -0500

    Finish 05_01

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	fmt.Println(addTwoValues(1, 2))
+	total, cnt := addAddValues(1, 2, 3, 4, 5)
+	fmt.Println(total, cnt)
+}
+
+func addTwoValues(val1 int, val2 int) int {
+	return val1 + val2
+}
+
+func addAddValues(values ...int) (int, int) {
+	var sum int
+	for _, val := range values {
+		sum += val
+	}
+	return sum, len(values)
 }
