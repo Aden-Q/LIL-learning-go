179752fd1b72308ca2e75d77bdf9a21cc337ed8b
Zecheng Qian <qian0102@umn.edu>
2022-06-18 11:47:50 -0500

    Finish 04_03

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,18 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println(sum)
+	}
 }
