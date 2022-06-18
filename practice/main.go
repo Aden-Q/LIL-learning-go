5b4e143c5e6638c37d14eccbb0abb916a079bd57
Zecheng Qian <qian0102@umn.edu>
2022-06-18 10:25:03 -0500

    Finish 03_05

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["1"] = "1"
+	states["2"] = "2"
+	states["3"] = "3"
+	fmt.Println(states)
+	delete(states, "1")
+	fmt.Println(states)
+
+	for k, v := range states {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("%v : %v\n", k, states[k])
+	}
 }
