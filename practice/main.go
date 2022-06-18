be0421fcdcc802649886b350527e447fa3373bcc
Zecheng Qian <qian0102@umn.edu>
2022-06-18 11:32:08 -0500

    Finish 04_01

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	if x := -42; x < 0 {
+		fmt.Println("Less than 0")
+	} else if x == 0 {
+		fmt.Println("Equal to 0")
+	} else {
+		fmt.Println("Greater than 0")
+	}
 }
