584f73624c38a3926b244d339ffd1d5d2cc9faac
Zecheng Qian <qian0102@umn.edu>
2022-06-18 12:40:16 -0500

    Finish 06_02

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Response type: %T\n", resp)
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	contents := string(bytes)
+	fmt.Print(contents)
 }
