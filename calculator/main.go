d0535fb4dcd1a30ad9835f38eb885d47ac11b77d
Zecheng Qian <qian0102@umn.edu>
2022-06-18 09:31:00 -0500

    Finish 02_08

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("A simple calculator")
+	input := bufio.NewReader(os.Stdin)
+	fmt.Print("Value 1: ")
+	val1str, _ := input.ReadString('\n')
+	val1, err := strconv.ParseFloat(strings.TrimSpace(val1str), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 1 parsing error")
+	}
+	fmt.Print("Value 2: ")
+	val2str, _ := input.ReadString('\n')
+	val2, err := strconv.ParseFloat(strings.TrimSpace(val2str), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 2 parsing error")
+	}
+	fmt.Printf("The sum of %.2f and %.2f is %.2f\n", val1, val2, val1+val2)
 }
