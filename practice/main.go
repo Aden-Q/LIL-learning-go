f8a52a48919f6f2b525d3e9049fe41912c4a79c6
Zecheng Qian <qian0102@umn.edu>
2022-06-18 11:35:39 -0500

    Finish 04_02

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,13 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var result string
+	switch dow {
+	case 1:
+		result = "Sunday"
+	default:
+		result = "Other day"
+	}
+	fmt.Println(result)
 }
